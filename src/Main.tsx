cc7a91cb78037ea12525afa36075470aaa70b640
klaidosHka <klaidukast17@gmail.com>
2024-01-25 15:20:58 +0200

    Run context menu initialization on addon install/browser start

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,16 +2,29 @@ import '../public/styles/index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Browser from 'webextension-polyfill';
 
-import run from './Addon.ts';
 import App from './components/App.tsx';
+import Google from './context-menu-entries/Google.ts';
+import Root from './context-menu-entries/Root.ts';
+import Youtube from './context-menu-entries/Youtube.ts';
+import YoutubeTrailer from './context-menu-entries/YoutubeTrailer.ts';
+import ContextMenuHandler from './handlers/ContextMenuHandler.ts';
 
-// Initiate addon logic
-run();
-
-// Render the app
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
+
+Browser.runtime.onInstalled.addListener(() => {
+    // Root
+    ContextMenuHandler.setRoot(new Root());
+
+    // Google
+    ContextMenuHandler.add(new Google());
+
+    // Youtube
+    ContextMenuHandler.add(new Youtube());
+    ContextMenuHandler.add(new YoutubeTrailer());
+});
