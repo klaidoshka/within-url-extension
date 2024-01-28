df2dd57c87baa9ca4854d8acf075c7b08a4b36a8
klaidosHka <klaidukast17@gmail.com>
2024-01-28 13:43:00 +0200

    Enable context menus on install/start-up (actually)

--- src/Main.tsx
+++ src/Main.tsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </React.StrictMode>
 );
 
-Browser.runtime.onInstalled.addListener(() => {
+const setup = () => {
     // Root
     ContextMenuHandler.setRoot(new Root());
 
@@ -27,4 +27,7 @@ Browser.runtime.onInstalled.addListener(() => {
     // Youtube
     ContextMenuHandler.add(new Youtube());
     ContextMenuHandler.add(new YoutubeTrailer());
-});
+};
+
+Browser.runtime.onInstalled.addListener(setup);
+Browser.runtime.onStartup.addListener(setup);
